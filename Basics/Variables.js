4796b59f1bea3a2ad50a9949b9287c76efafce41
Sahilbabar0403 <121351087+Sahilbabar0403@users.noreply.github.com>
2025-06-21 10:02:35 +0000

    Variables done

--- Basics/Variables.js
+++ Basics/Variables.js
@@ -1,7 +1,18 @@
 const id=12025
 let name = "sahil"
 var city="pune"
+/*prefer not to ue var due to block scope problem */
+
 let state ;
+
+
+console.table([id,name,city,state])
+name="Vishal"
+city="Hyd"
+ state="Tel";
+
+// JS is a casesensative language always remember to check the variables name when assigning values
+
 
 
 console.table([id,name,city,state])
